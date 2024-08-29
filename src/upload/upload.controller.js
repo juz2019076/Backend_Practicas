95ff9d8ce06ac520b6f0ab50ab8f2e653e3b9e6a
juz2019076 <juz-2019076@kinal.edu.gt>
2024-08-29 13:20:14 -0600

    Se corrigen archivos

--- src/upload/upload.controller.js
+++ src/upload/upload.controller.js
@@ -10,16 +10,14 @@ const __dirname = path.dirname(__filename);
 export const handleFileUpload = async (req, res) => {
     try {
         const files = req.files;
-        const md5FilePath = path.join(__dirname, '../2checksum.md5');
+        const md5FilePath = path.join(__dirname, '../3checksum.md5');
 
-        // Leer el archivo checksum.md5
         const checksumData = fs.readFileSync(md5FilePath, 'utf8');
         const expectedChecksums = checksumData.split('\n').filter(line => line.trim() !== '');
 
         for (let file of files) {
             const fileContent = fs.readFileSync(file.path, 'utf8');
 
-            // Verificar si el archivo es un JSON válido
             let jsonData;
             try {
                 jsonData = JSON.parse(fileContent);
@@ -27,13 +25,10 @@ export const handleFileUpload = async (req, res) => {
                 return res.status(400).json({ message: `El archivo ${file.originalname} no es un JSON válido.` });
             }
 
-            // Calcular el MD5 del contenido del archivo
             const md5sum = crypto.createHash('md5').update(fileContent).digest('hex');
 
-            // Debug: Mostrar el MD5 calculado
             console.log(`MD5 Calculado para ${file.originalname}: ${md5sum}`);
 
-            // Buscar el checksum esperado para este archivo en checksum.md5
             const originalChecksum = expectedChecksums.find(line => line.includes(file.originalname));
 
             if (originalChecksum) {
@@ -42,12 +37,10 @@ export const handleFileUpload = async (req, res) => {
                 console.log(`No se encontró un checksum para ${file.originalname} en el archivo checksum.md5`);
             }
 
-            // Verificar si el checksum coincide
             if (!originalChecksum || !originalChecksum.includes(md5sum)) {
                 return res.status(400).json({ message: `El archivo ${file.originalname} está corrupto o no coincide con el checksum.` });
             }
 
-            // Crear un nuevo registro en la base de datos
             const newRegistro = new Registro({
                 file: file.originalname,
                 fecha_creacion: new Date(),
@@ -55,14 +48,11 @@ export const handleFileUpload = async (req, res) => {
                 data: jsonData
             });
 
-            // Guardar el registro en la base de datos
             await newRegistro.save();
 
-            // Eliminar el archivo subido temporalmente
             fs.unlinkSync(file.path);
         }
 
-        // Responder con éxito
         res.status(200).json({ message: 'Archivos procesados exitosamente.' });
 
     } catch (error) {
